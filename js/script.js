88f2da05b3da029936a52066e107aed8b9edf743
archana vinayagam <saiarchana.93@gmail.com>
2021-12-19 13:40:05 +0530

    Added functions to play and pause buttons

--- js/script.js
+++ js/script.js
@@ -13,8 +13,8 @@ const playList = container.querySelector(".playlist");
 const musicListBtn = container.querySelector("#music-list");
 const closeMusicList = container.querySelector("#closelist");
 
-// load music function
 
+// load music function
 function loadSongs(indexNumb) {
     songName.innerText = allSongs[indexNumb - 1].name;
     songArtist.innerText = allSongs[indexNumb - 1].artist;
@@ -22,3 +22,53 @@ function loadSongs(indexNumb) {
     mainSong.src = `./songs/${allSongs[indexNumb - 1].src}.mp3`;
 }
 
+// play music function
+function playAudio() {
+    container.classList.add("paused");
+    playpauseBtn.innerHTML = '<i class="fa fa-pause">';
+    mainSong.play();
+}
+
+// pause music function
+function pauseAudio() {
+    container.classList.remove("paused");
+    playpauseBtn.innerHTML= '<i class="fa fa-play">';
+    mainSong.pause();
+}
+
+// Next Music function
+function nextSong() {
+    songIndex++; //increment of musicIndex by 1
+    // if musicIndex is greater than array length then musicIndex will be 1 so the first music play
+    songIndex > allSongs.length ? songIndex = 1 : songIndex = songIndex;
+    loadSongs(songIndex);
+    playAudio();
+    playingMusic();
+}
+
+// Prev Music function
+function prevSong() {
+    songIndex--; 
+    songIndex < 1 ? songIndex = allSongs.length : songIndex = songIndex;
+    loadSongs(songIndex);
+    playAudio();
+    playingMusic();
+}
+
+// play or Pause button event
+playpauseBtn.addEventListener("click", () => {
+    const isAudioPaused = container.classList.contains("paused");
+    isAudioPaused ? pauseAudio() : playAudio();
+
+});
+
+// next music button event
+nextBtn.addEventListener("click", () => {
+    nextSong();
+});
+
+
+// prev music button event
+prevBtn.addEventListener("click", () => {
+    prevSong();
+});
