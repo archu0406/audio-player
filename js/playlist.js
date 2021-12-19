d3e0b34c6cf7c549b3572febfdf2be50aa839476
archana vinayagam <saiarchana.93@gmail.com>
2021-12-19 12:43:08 +0530

    Update playlist array to the musicplayer

---
+++ js/playlist.js
@@ -0,0 +1,65 @@
+let allSongs = [
+
+    {
+        name: "Athousandyears",
+        artist: "Christina-Perri",
+        img: "Img1",
+        src: "Athousandyears"
+    },
+    {
+        name: "Attention",
+        artist: "Charlie-puth",
+        img: "Img2",
+        src: "Attention"
+    },
+    {
+        name: "Cheapthrills",
+        artist: "Sia-ft-Sean-Paul",
+        img: "Img3",
+        src: "Cheapthrills"
+    },
+    {
+        name: "Despacito",
+        artist: "Luis-fonsi-ft-Daddy-yankee",
+        img: "Img4",
+        src: "Despacito"
+    },
+    {
+        name: "Love me like you do",
+        artist: "Ellie-goulding",
+        img: "Img5",
+        src: "Lovemelikeyoudo"
+    },
+    {
+        name: "Lovestory",
+        artist: "Taylor-swift",
+        img: "Img6",
+        src: "Lovestory"
+    },
+    {
+        name: "Shapeofyou",
+        artist: "Ed-sheeran",
+        img: "Img7",
+        src: "Shapeofyou"
+    },
+    {
+        name: "Unfaithful",
+        artist: "Rihanna",
+        img: "Img8",
+        src: "Unfaithful"
+    },
+    {
+        name: "Wedonttalkanymore",
+        artist: "Charlie-puth-selena-gomez",
+        img: "Img9",
+        src: "Wedonttalkanymore"
+    },
+    {
+        name: "Whosays",
+        artist: "Selena-gomez",
+        img: "Img10",
+        src: "Whosays"
+    }
+    
+    ];
+
